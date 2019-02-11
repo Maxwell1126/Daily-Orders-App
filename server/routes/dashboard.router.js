0a2217c5a925e8231b1ff33d29017b5a2528fcd0
Maxwell Todd <mtmaxtodd@gmail.com>
2019-02-11 11:55:36 -0600

    added dashboard router and got a get to the router to work on postman.

---
+++ server/routes/dashboard.router.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+    pool.query(`SELECT * FROM "order";`).then((results)=>{
+        res.send(results.rows);
+    }).catch((error)=>{
+        console.log('error in dashboard get', error);
+        res.sendStatus(500);
+    })
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
