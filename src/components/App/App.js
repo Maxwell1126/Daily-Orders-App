52f9b57156b160e543984839f4373ed8fbf7ea3a
Maxwell Todd <mtmaxtodd@gmail.com>
2019-02-11 14:02:13 -0600

    UserPage now displays the order names from the order table on the dom.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,6 +17,7 @@ import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
 
+
 import './App.css';
 
 class App extends Component {
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React , {Component} from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
+import OrderSheet from '../OrderSheet/OrderSheet';
 import axios from 'axios';
 
+class UserPage extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      orders:[],
+    }
+  }
+
+  componentDidMount(){
+    this.getOrders();
+  }
+getOrders = (event) => {
+  axios.get('api/dashboard').then(response => {
+    this.setState({
+      ...this.state,
+      orders: response.data,
+    })
+  })
+}
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
-const UserPage = (props) => (
-  <div>
-    <h1 id="welcome">
-      Welcome, { props.user.username }!
-    </h1>
-    <p>Your ID is: {props.user.id}</p>
-    <LogOutButton className="log-in" />
-  </div>
-);
-
+// this.render
+// 
+render(){
+  return(
+    <div>
+     <h1 id="welcome">
+       Welcome, { this.props.user.username }!
+     </h1>
+      <p>Your Orders:</p>
+      <p>{this.state.orders.map((order) => {
+        return (order.order_name)
+      })}</p>
+     <LogOutButton className="log-in" />
+   </div>
+  )
+}
+}
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({user}) => ({ user });
