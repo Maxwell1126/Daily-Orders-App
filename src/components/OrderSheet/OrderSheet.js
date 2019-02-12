e0b7955744a2728a00697dee4f362b2ee22e2f53
Maxwell Todd <mtmaxtodd@gmail.com>
2019-02-12 14:18:16 -0600

    added child component for ordersheet to house the individual products. Added functions that allow for the quantity to be changed in the local state of ordersheet. Quantites are displayed on the dom.

--- src/components/OrderSheet/OrderSheet.js
+++ src/components/OrderSheet/OrderSheet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import axios from 'axios';
+import OrderSheetItem from './OrderSheetItem';
 
 class OrderSheet extends Component {
     constructor(props) {
@@ -23,26 +24,49 @@ class OrderSheet extends Component {
             })
         })
     }
+
     // this could also be written with destructuring parameters as:
     // const UserPage = ({ user }) => (
     // and then instead of `props.user.username` you could use `user.username`
     // this.render
     // 
-
-    upCount = ()=>{
-        console.log('this',this);
-        
+    upQuantity = i => {
+        //Credit for this function belongs to Dereje1 from this link:
+        //https://www.freecodecamp.org/forum/t/reactjs-using-setstate-to-update-a-single-property-on-an-object/146772
+        let productsCopy = JSON.parse(JSON.stringify(this.state.products))
+        productsCopy[i].quantity ++;
+            this.setState({
+                products: productsCopy
+            })
+        }
+    downQuantity = (i) => {
+        //Credit for this function belongs to Dereje1 from this link:
+        //https://www.freecodecamp.org/forum/t/reactjs-using-setstate-to-update-a-single-property-on-an-object/146772
+        if(this.state.products[i].quantity!==0){
+        let productsCopy = JSON.parse(JSON.stringify(this.state.products))
+        productsCopy[i].quantity--;
+        this.setState({
+            products: productsCopy
+        })
     }
-
+    }
     render() {
         return (
             <div>
-                <p>{JSON.stringify(this.state.products)}</p>
+                {/* <p>{JSON.stringify(this.state.products[0])}</p>
+                <p>{JSON.stringify(this.state.products[1])}</p>
+                <p>{JSON.stringify(this.state.products[2])}</p> */}
                 <p>Order Sheet</p>
                 {/* <p>{JSON.stringify(this.props)}</p> */}
-                <div>{this.state.products.map((product) => {
-                    return (<div>{product.product_name} 
-                            <button onClick={this.upCount}>+</button>{product.quantity}<button>-</button></div>)
+                <div>{this.state.products.map((product, i) => {
+                    console.log('product',product);
+                    return (<OrderSheetItem
+                        history={this.props.history}
+                        product={product}
+                        i={i}
+                        upQuantity={this.upQuantity}
+                        downQuantity={this.downQuantity}
+                        getProducts={this.getProducts}/>)
                 })}</div>
                 <button>Submit</button>
                 <br></br>
---
+++ src/components/OrderSheet/OrderSheetItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+class OrderSheet extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+        this.props.getProducts();
+    }
+    // this could also be written with destructuring parameters as:
+    // const UserPage = ({ user }) => (
+    // and then instead of `props.user.username` you could use `user.username`
+    // this.render
+    // 
+
+    upCount = () => {
+        this.props.upQuantity(this.props.i)        
+    }
+    downCount = () => {
+        this.props.downQuantity(this.props.i)
+    }
+
+    render() {
+        return (<div>{this.props.product.product_name}
+            <button onClick={this.upCount}>+</button>
+            {this.props.product.quantity}
+            <button onClick={this.downCount}>-</button></div>)
+    }
+}
+// Instead of taking everything from state, we just want the user info.
+// if you wanted you could write this code like this:
+// const mapStateToProps = ({user}) => ({ user });
+const mapStateToProps = state => ({
+    user: state.user,
+});
+
+// this allows us to use <App /> in index.js
+export default connect(mapStateToProps)(OrderSheet);
